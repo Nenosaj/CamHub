c84d3c92df52be4721e49c494770fbda093bfead
Nenosaj <jasondaohog1@gmail.com>
2024-09-21 13:15:55 +0800

    Admin Interface

---
+++ lib/screens/AdminUI/admin_ui.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+//import 'dashboardadmin.dart';
+
+class AdminUI extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Admin Interface')),
+      //body: Dashboard(), // Your dashboard content
+    );
+  }
+}
