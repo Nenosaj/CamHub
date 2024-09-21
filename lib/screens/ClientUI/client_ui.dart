448ab5d9f6f5773f7ff508593bc9422b61192d85
CREYAJ <apuya.christianjames@ustp.edu.ph>
2024-09-22 02:10:47 +0800

    loginscreen done

--- lib/screens/ClientUI/client_ui.dart
+++ lib/screens/ClientUI/client_ui.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
 class ClientUI extends StatelessWidget {
+  const ClientUI({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Client Interface')),
+      appBar: AppBar(title: const Text('Client Interface')),
     );
   }
 }
