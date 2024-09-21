cb9bd68460d8dd76aa176c5064572644cc188a1f
Czarshan <balucosczar@gmail.com>
2024-09-22 03:20:16 +0800

    ClientUI changed

--- lib/screens/ClientUI/client_ui.dart
+++ lib/screens/ClientUI/client_ui.dart
@@ -2,12 +2,22 @@ import 'package:flutter/material.dart';
 import 'clienthomepage.dart'; // Import the homepage.dart file
 
 class ClientUI extends StatelessWidget {
-  const ClientUI({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Client Interface')),
+    return MaterialApp(
+      title: 'Role-Based App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // Start with the HomePage or other routes
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomePage(), // HomePage as the default route
+        //'/chat': (context) => ChatPage(), // Define ChatPage in a separate file
+        //'/bookings': (context) => BookingsPage(), // Define BookingsPage in a separate file
+        //'/notifications': (context) => NotificationsPage(), // Define NotificationsPage in a separate file
+        //'/profile': (context) => ProfilePage(), // Define ProfilePage in a separate file
+      },
     );
   }
 }
