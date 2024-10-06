44eace7f7a95c0be760f771b476521a5976bcf2f
Nenosaj <jasondaohog1@gmail.com>
2024-10-06 14:06:48 +0800

    Debugging gone

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Role-Based App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
